a017f2daa88544566b7167cbfd88b12d5d1fb878
jerbi86 <ahmedjerbi86@gmail.com>
2025-10-31 01:12:44 +0100

    fix: another ssl config

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -3,16 +3,16 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA', 'public'),
       ssl: env.bool('DATABASE_SSL', true)
         ? (env('DATABASE_CA')
-            ? { ca: env('DATABASE_CA') }               // verify using provided CA
-            : { rejectUnauthorized: false })           // fallback if CA not provided
+            ? { ca: env('DATABASE_CA') }       
+            : { rejectUnauthorized: false })   
         : false,
     },
     pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
