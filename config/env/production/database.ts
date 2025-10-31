62ea348011252d7564ae8065fd4ae3c781cbb94e
jerbi86 <ahmedjerbi86@gmail.com>
2025-10-31 02:43:50 +0100

    fix: trying default config

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -3,17 +3,15 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      host: env('DATABASE_HOST'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      schema: env('DATABASE_SCHEMA', 'public'),
-      ssl: env.bool('DATABASE_SSL', true)
-        ? (env('DATABASE_CA')
-            ? { ca: env('DATABASE_CA') }       
-            : { rejectUnauthorized: false })   
-        : false,
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env.bool('DATABASE_SSL', false) && {
+        ca: env('DATABASE_CA')
+      },
     },
     pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     debug: false,
