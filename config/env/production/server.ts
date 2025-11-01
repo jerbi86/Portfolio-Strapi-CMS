1e578f69124d068e12ed63ce2298495bc7530cd2
jerbi86 <ahmedjerbi86@gmail.com>
2025-11-01 06:14:57 +0100

    fix: enabled strapi transfer

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -2,6 +2,10 @@
 export default ({ env }) => ({
         proxy: { koa: true },
         url: env('APP_URL'), // Sets the public URL of the application.
+        transfer: {
+          token: { salt: env('TRANSFER_TOKEN_SALT') },
+          remote: { enabled: true }, // <— allow remote data transfer
+        },
         app: { 
           keys: env.array('APP_KEYS')
         },
