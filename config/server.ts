ec064c33be534f6b85888442399bd392ef8b4dd0
jerbi86 <ahmedjerbi86@gmail.com>
2025-11-01 02:53:19 +0100

    fix: production settings from strapi docs

--- config/server.ts
+++ config/server.ts
@@ -1,13 +1,7 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
-    },
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
